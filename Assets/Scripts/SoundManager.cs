7491f1b976784ce86f108c547e0f9e53500494b3
kakorolling <kakorolling@gmail.com>
2024-10-25 05:24:15 +0900

    StartScene

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,5 +2,27 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+
+    public static SoundManager instance;
+
+    public AudioSource bgmPlayer;
+    public AudioSource sePlayer;
+    public AudioClip[] seClips;
     
+    void Awake()
+    {
+        instance = this;
+    }
+    
+     public void PlaySESound(string type)
+    {
+        int index = 0;
+
+        switch (type) {
+            case "DecideBtn": index = 0; break;
+        }
+
+        sePlayer.clip = seClips[index];
+        sePlayer.Play();
+    }
 }
--- Assets/Scripts/StartManager.cs
+++ Assets/Scripts/StartManager.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,12 +9,12 @@ public class StartManager : MonoBehaviour
 {
 
     public GameObject startBtn;
-
     public GameObject shadow;
     public GameObject signinWindow;
+    public GameObject alertWindow;
 
-    [SerializeField] InputField emailField;
-    [SerializeField] InputField pwField;
+    [SerializeField] TMP_InputField emailInput;
+    [SerializeField] TMP_InputField pwInput;
 
     // 인증을 관리할 객체
     Firebase.Auth.FirebaseAuth auth;
@@ -25,29 +27,54 @@ public class StartManager : MonoBehaviour
 
     void Start()
     {
+        startBtn.GetComponent<Animator>().Play("StartScene_StartBtn_Show");
         startBtn.SetActive(true);
         shadow.SetActive(false);
-        signinWindow.SetActive(true);
+        signinWindow.SetActive(false);
+        alertWindow.SetActive(false);
 
     }
 
     public void PushStartBtn(){
+        SoundManager.instance.PlaySESound("DecideBtn");
+        startBtn.SetActive(false);
+        shadow.SetActive(true);
+        signinWindow.SetActive(true);
+        
+    }
+
+    public void PushBackBtn(){
+        SoundManager.instance.PlaySESound("DecideBtn");
+        shadow.SetActive(false);
+        signinWindow.SetActive(false);
+        startBtn.SetActive(true);
+    }
+
+    public void PushLoginBtn(){
+        SoundManager.instance.PlaySESound("DecideBtn");
         Login();
+    }
+
+    public void PushAlertBackBtn(){
+        SoundManager.instance.PlaySESound("DecideBtn");
+        signinWindow.SetActive(true);
     }
 
     public void Login()
     {
         // 제공되는 함수 : 이메일과 비밀번호로 로그인 시켜 줌
-        auth.SignInWithEmailAndPasswordAsync(emailField.text, pwField.text).ContinueWith(
+        auth.SignInWithEmailAndPasswordAsync(emailInput.text, pwInput.text).ContinueWith(
             task => {
                 if (task.IsCompleted && !task.IsFaulted && !task.IsCanceled)
                 {
                     SceneManager.LoadScene("MainScene");
-                    Debug.Log(emailField.text + " 로 로그인 하셨습니다.");
+                    Debug.Log(emailInput.text + " 로 로그인 하셨습니다.");
                 }
                 else
                 {
-                    Debug.Log("로그인에 실패하셨습니다.");
+                    //Debug.Log("로그인 실패");
+                    alertWindow.SetActive(true);
+                    signinWindow.SetActive(false);
                 }
             }
         );
